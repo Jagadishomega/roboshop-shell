fc8fad2e00cbf3cc870c2531a8018f0bbf4f0712
Jagadish <Jagadishdevops.omega@gmail.com>
2023-08-28 13:39:59 +0530

    automated roboshop shell-scripts

--- mongod.sh
+++ mongod.sh
@@ -2,7 +2,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
-echo "Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf"
+#echo "Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf"
 sed -i 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
 
 systemctl enable mongod
