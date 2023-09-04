82aa5718704ba23d9b0830bfb2093b752ab16ff6
Jagadish <Jagadishdevops.omega@gmail.com>
2023-09-04 16:07:09 +0530

    Added Terraform code

--- ax_teraform/ax_server.tf
+++ ax_teraform/ax_server.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "Ajax-FC"
+    Name = "HelloWorld"
   }
 }
